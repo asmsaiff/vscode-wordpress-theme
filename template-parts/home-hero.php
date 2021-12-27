2c2ed34998ced761eefc116db1465a46353acf08
saidmsaif <saidmsaiff@gmail.com>
2021-12-27 22:35:36 +0600

    seperate home hero, home meta, and technology icons

---
+++ template-parts/home-hero.php
@@ -0,0 +1,14 @@
+<div class="hero px-2 px-md-5 pt-5 pt-md-0">
+    <div class="row align-items-center hero-wrapper">
+        <div class="col-md-7">
+            <h1 class="hero-title fw-bold">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+
+            <p class="lh-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aspernatur voluptatem modi totam excepturi quidem sed libero, eaque laborum nemo nisi temporibus recusandae odit eveniet autem ea molestiae sunt voluptatum.</p>
+
+            <p class="lh-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aspernatur voluptatem modi totam excepturi quidem sed libero, eaque laborum nemo nisi temporibus recusandae odit eveniet autem ea molestiae sunt voluptatum.</p>
+        </div>
+        <div class="col-md-5 text-center">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/programmer.png" class="img-fluid" alt="">
+        </div>
+    </div>
+</div>
