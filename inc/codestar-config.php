dc813fdabe2b60be1a158812962bebf960d8cf2a
saidmsaif <saidmsaiff@gmail.com>
2021-12-27 23:09:49 +0600

    add codester and initialize option panel

---
+++ inc/codestar-config.php
@@ -0,0 +1,22 @@
+<?php
+    if( class_exists( 'CSF' ) ) {
+        $prefix = 'diww';
+      
+        CSF::createOptions( $prefix, array(
+            'menu_title'    => 'DIWW Options',
+            'menu_slug'     => 'diww_options',
+        ));
+      
+        CSF::createSection( $prefix, array(
+            'title'           => 'General',
+            'fields'            =>  array(
+                array(
+                    'title'             =>  __('Title', 'diww'),
+                    'id'                =>  'title',
+                    'type'              =>  'text'
+                )
+            ),
+        ));
+      
+        include_once get_template_directory().'/inc/options/home-hero.php';
+    }
---
+++ inc/options/home-hero.php
@@ -0,0 +1,11 @@
+<?php
+    CSF::createSection( $prefix, array(
+        'title' => 'Home Hero',
+        'fields'                =>  array(
+            array(
+                'title'         =>  __('Home Hero Title', 'diww'),
+                'id'            =>  'home_hero_title',
+                'type'          =>  'text'
+            )
+        ),
+    ));
