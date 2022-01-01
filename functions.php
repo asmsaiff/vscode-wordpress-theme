947191e7d01aa3423b97be0454a19805e9d98ff1
saidmsaif <saidmsaiff@gmail.com>
2022-01-01 23:25:54 +0600

    add theme assets CDN file to functions

--- functions.php
+++ functions.php
@@ -15,11 +15,15 @@
     add_action('after_setup_theme', 'diww_theme_setup');
 
     function diww_assets_enqueue() {
+        wp_enqueue_style('remixicon-stylesheet', '//cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css');
+        wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&family=Parisienne&family=Philosopher:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+		wp_enqueue_style('bootstrap-stylesheet', '//cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
+		wp_enqueue_style('highlightjs-stylesheet', '//cdn.jsdelivr.net/npm/highlightjs-themes@1.0.0/solarized_dark.css');
         wp_enqueue_style('style-stylesheet', get_template_directory_uri().'/assets/css/style.css');
         wp_enqueue_style('responsive-stylesheet', get_template_directory_uri().'/assets/css/responsive.css');
-		wp_enqueue_style('highlightjs-stylesheet', 'https://cdn.jsdelivr.net/npm/highlightjs-themes@1.0.0/solarized_dark.css');
         wp_enqueue_style('main-stylesheet', get_stylesheet_uri());
 
+		wp_enqueue_script('bootstrap-js', '//cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', array('jquery'), time(), true);
 		wp_enqueue_script('highlight-js', '//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.3.1/build/highlight.min.js', array('jquery'), time(), true);
         wp_enqueue_script('main-js', get_template_directory_uri().'/assets/js/main.js', array('jquery'), time(), true);
     }
